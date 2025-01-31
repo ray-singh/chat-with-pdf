6c18d3bb45af7822265a7b5cc20611178d99f47c
ray-singh <rayansh365@gmail.com>
2025-01-31 15:15:20 -0500

    Connected AWS S3 for file upload

--- chatpdf/src/components/FileUpload.tsx
+++ chatpdf/src/components/FileUpload.tsx
@@ -1,4 +1,5 @@
 'use client'
+import { uploadToS3 } from '@/lib/s3'
 import { Inbox } from 'lucide-react'
 import React from 'react'
 import {useDropzone} from 'react-dropzone'
@@ -7,8 +8,20 @@ const FileUpload = () => {
     const {getRootProps, getInputProps} = useDropzone({
         accept : {'application/pdf' : ['.pdf']},
         maxFiles: 1,
-        onDrop: (acceptedFiles) => {
+        onDrop: async (acceptedFiles) => {
             console.log(acceptedFiles);
+            const file = acceptedFiles[0]
+            if (file.size > 10 * 1024 * 1024){
+                alert('Files larger than 10MB are not allowed')
+                return
+            }
+            
+            try {
+                const data = await uploadToS3(file);
+                console.log('Data', data)
+            } catch (error) {
+                console.log(error);
+            }
         }
     })
   return (
